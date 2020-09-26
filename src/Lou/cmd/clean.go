7f3285d096fc9b6c638310f16640adda605d0fb1
DanielRivasMD <danielrivasmd@gmail.com>
2020-09-26 21:49:54 +0200

    add lou clean. dupClean

---
+++ src/Lou/cmd/clean.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2020 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"log"
+
+	"lineBreaks"
+	"shellCall"
+
+	"github.com/labstack/gommon/color"
+
+	"github.com/spf13/cobra"
+)
+
+// cleanCmd represents the clean command
+var cleanCmd = &cobra.Command{
+	Use:   "clean",
+	Short: "Clean purges downloaded duplicates",
+	Long:  `Clean purges downloaded duplicates`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(cmd *cobra.Command, args []string) {
+		// lineBreaks
+		lineBreaks.LineBreaks()
+
+		// shellCall
+		err, out, errout := shellCall.ShellCall("/Users/drivas/Factorem/Lou/src/Lou/shell/dupClean.sh")
+		if err != nil {
+			log.Printf("error: %v\n", err)
+		}
+
+		// stdout
+		color.Println(color.Cyan(out, color.B))
+
+		// stderr
+		if errout != "" {
+			color.Println(color.Red(errout, color.B))
+		}
+
+		// lineBreaks
+		lineBreaks.LineBreaks()
+
+	},
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+
+func init() {
+	rootCmd.AddCommand(cleanCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cleanCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cleanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ src/Lou/shell/dupClean.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################################################################################
+
+function cleaning() {
+  # duplicates to clean at ~/Downloads/
+  dups_to_clean=$( ls ~/Downloads/*\(*\)* 2>&1 )
+
+  expectedError="No such file or directory"
+  if [[ ${dups_to_clean} =~ ${expectedError} ]]
+  then
+    emptyMessage=`cat <<- TESTA
+\t\t\tThere were no files to remove
+TESTA`
+
+    echo -e "${emptyMessage}"
+  else
+    removingFiles=`cat <<- TESTA
+\tFiles to remove:
+${dups_to_clean}
+TESTA`
+
+    echo -e "${removingFiles}"
+    rm /Users/drivas/Downloads/*\(*\)*
+  fi
+}
+
+################################################################################
+
+cleaning
+
+################################################################################
