332f9afb9451d4d208e497bb6f7890b54caaa772
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-15 13:52:12 -0600

    add caps lock shell

---
+++ sh/caps_lock.sh
@@ -0,0 +1,5 @@
+####################################################################################################
+
+sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
+
+####################################################################################################
