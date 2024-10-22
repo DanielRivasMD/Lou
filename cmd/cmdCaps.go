ef66aa84565f6a249b2698213e21bfe4a6031948
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-21 20:24:58 -0600

    add caps sub cmd

---
+++ cmd/cmdCaps.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2024 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// capsCmd
+var capsCmd = &cobra.Command{
+
+	Use:   "caps",
+	Short: "" + chalk.Yellow.Color("cobra") + ".",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("tabularasa") + ` help ` + chalk.Yellow.Color("caps"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	osCmd.AddCommand(capsCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
