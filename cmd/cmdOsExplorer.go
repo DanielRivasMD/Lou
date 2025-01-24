ac51468124cb35d07c3d349398be199caf0f45e5
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-23 18:31:26 -0600

    add explorer os cmd

---
+++ cmd/cmdOsExplorer.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// explorerCmd
+var explorerCmd = &cobra.Command{
+	Use:   "explorer",
+	Short: "" + chalk.Yellow.Color("") + ".",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("lou") + ` help ` + chalk.Yellow.Color("os") + chalk.Yellow.Color("explorer"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(κ *cobra.Command, args []string) {
+
+		cmd := "zellij run -- lsd --header --long --classify --git $(pwd)/" + args[0]
+
+		fmt.Println(cmd)
+		shcmd := cmd
+		ε, _, _ := shellCall(shcmd)
+		checkErr(ε)
+
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	osCmd.AddCommand(explorerCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
