81bdf49fff0c33002f9e00cd511744b0f4a39f6e
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-22 15:45:50 -0600

    update explorer shell

--- sh/explorer.sh
+++ sh/explorer.sh
@@ -5,18 +5,25 @@
 cmd=$1
 file=$2
 
-if [[ "$cmd" == "lsd" ]]; then
-  cmd="lsd --header --long --classify --git"
+if [[ $1 == "lsd" ]]; then
+  zellij run \
+    --name canvas \
+    --floating \
+    --height 100 \
+    --width 100 \
+    --x 100 \
+    --y 0 \
+    -- "$cmd" --header --long --classify --git "$file"
+else
+  zellij run \
+    --name canvas \
+    --floating \
+    --height 100 \
+    --width 100 \
+    --x 100 \
+    --y 0 \
+    -- "$cmd" "$file"
 fi
 
-# run
-zellij run \
-  --name canvas \
-  --floating \
-  --height 100 \
-  --width 100 \
-  --x 100 \
-  --y 0 \
-  -- "$cmd" "$file"
 
 ################################################################################
