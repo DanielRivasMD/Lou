d03133f37efab495d5bf12070e547d0ff7d89b5c
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-24 11:19:48 -0600

    add diff cmd

---
+++ cmd/cmdDiff.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// diffCmd
+var diffCmd = &cobra.Command{
+	Use:   "diff",
+	Short: "" + chalk.Yellow.Color("") + ".",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("diff"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(κ *cobra.Command, args []string) {
+
+		 cmdDiff := "git diff --name-only --relative --diff-filter=d | xargs bat --diff"
+		 shellCall(cmdDiff)
+	},
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(diffCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
