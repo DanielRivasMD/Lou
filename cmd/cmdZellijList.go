fc445df34feaaabf561d14025b0f568b5346ebc3
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-25 22:30:19 -0600

    add zellij list cmd

---
+++ cmd/cmdZellijList.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// zellijlistCmd
+var zellijlistCmd = &cobra.Command{
+	Use:   "list",
+	Aliases: []string{"k"},
+	Short: "List Zellij sessions",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+
+` + chalk.Green.Color("Lou") + ` display Zellij sessions
+`,
+
+	Example: `
+` + chalk.Cyan.Color("lou") + ` ` + chalk.Yellow.Color("list") + `
+`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(κ *cobra.Command, args []string) {
+
+		// command
+		cmdZellijList := `zellij list-sessions`
+		shellCall(cmdZellijList)
+	},
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(zellijlistCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
