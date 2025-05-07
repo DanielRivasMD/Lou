5c6027d2f87b1c9c731eee31420aae2650d6d8da
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-07 14:50:19 -0600

    add lsd cmd

---
+++ cmd/cmdLsd.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// lsdCmd
+var lsdCmd = &cobra.Command{
+	Use:     "lsd",
+	Aliases: []string{"l"},
+	Short:  "View data in a floating Zellij window using lsd",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("lsd"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(κ *cobra.Command, args []string) {
+
+		// base command
+		cmdView := `zellij run --name canvas --floating --height 100 --width 100 --x 100 --y 0 -- `
+		cmdView += `lsd  --header --long --classify --git`
+
+		// validate input
+		arg := args[0]
+
+		// execute command
+		cmdView += " " + arg
+		shellCall(cmdView)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(lsdCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
