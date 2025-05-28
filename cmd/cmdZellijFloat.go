3e2b765ec792b35dbecd9589f3d3089e0c4137b8
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-28 10:23:55 +0200

    add zellij float cmd

---
+++ cmd/cmdZellijFloat.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/DanielRivasMD/domovoi"
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// floatCmd
+var floatCmd = &cobra.Command{
+	Use:   "float",
+	Short: "Launch floating Zellij window with ease",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("bat"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// base command
+		cmdFloat := `zellij run --name canvas --floating --height 100 --width 100 --x 100 --y 0 -- `
+		cmdFloat += `zsh`
+
+		// execute command
+		domovoi.ExecCmd("bash", "-c", cmdFloat)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(floatCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
