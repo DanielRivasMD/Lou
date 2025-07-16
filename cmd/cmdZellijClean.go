fe1b21d28968e6e04010d220cb4cdd26c45825a9
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-16 15:46:05 +0200

    update zellij clean cmd

---
+++ cmd/cmdZellijClean.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/DanielRivasMD/domovoi"
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// zellijCleanCmd
+var zellijCleanCmd = &cobra.Command{
+	Use:   "clean",
+	Short: "" + chalk.Yellow.Color("") + ".",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("zellij") + chalk.Yellow.Color("clean"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(κ *cobra.Command, args []string) {
+
+		// command
+		cmdZellijList := `zellij delete-all-sessions --yes`
+		domovoi.ExecCmd("bash", "-c", cmdZellijList)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	zellijCmd.AddCommand(zellijCleanCmd)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
