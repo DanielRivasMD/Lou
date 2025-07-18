0806d806e5ca8abdd1886206a4a0bbaf54c410b7
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-18 09:46:15 +0200

    deprecate caps lock sh

--- sh/caps_lock.sh
+++
@@ -1,5 +0,0 @@
-####################################################################################################
-
-sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
-
-####################################################################################################
