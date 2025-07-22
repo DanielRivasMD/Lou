c51f1935433cb7f11a676d2f6aa3f1afe259bfc5
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-22 14:44:56 +0200

    add zellij broot cmd

---
+++ cmd/cmdZellijBroot.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/DanielRivasMD/domovoi"
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// declarations
+var ()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// zBrootCmd
+var zBrootCmd = &cobra.Command{
+	Use:     "broot",
+	Aliases: []string{"br"},
+	Short:   "Lazygit in a floating Zellij window",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("helix"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// base command
+		cmdBroot := `zellij run --name canvas --close-on-exit --floating --pinned true --height 100 --width 130 --x 10 --y 0 -- `
+		cmdBroot += `broot --dates --sizes --permissions --hidden --git-ignored --show-git-info --sort-by-type-dirs-first`
+
+		// execute command
+		domovoi.ExecCmd("zsh", "-c", cmdBroot)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(zBrootCmd)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
