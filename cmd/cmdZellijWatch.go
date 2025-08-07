feec9a896f289b0c78db0b59b304ed539d7edc4c
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-07 13:50:38 +0200

    update doc zellij watch cmd

--- cmd/cmdZellijWatch.go
+++ cmd/cmdZellijWatch.go
@@ -16,7 +16,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package cmd
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import (
+	"fmt"
+
 	"github.com/DanielRivasMD/domovoi"
 	"github.com/spf13/cobra"
 	"github.com/ttacon/chalk"
@@ -24,38 +28,41 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// declarations
 var ()
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// zWatchCmd
 var zWatchCmd = &cobra.Command{
 	Use:   "watch",
-	Short: "Launch watcher on a floating Zellij window with ease",
-	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+	Short: `launch watcher on a floating zellij window with ease`,
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) +
+		chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+
+` +
+		`launch a ` + chalk.Cyan.Color(chalk.Italic.TextStyle("watcher")) + ` on a floating ` + chalk.Cyan.Color(chalk.Italic.TextStyle("zellij")) + ` window with ease
 `,
 
-	Example: `
-` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("bat"),
+	Example: chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("watch")),
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	Run: func(cmd *cobra.Command, args []string) {
-
 		// TODO: test whether `just watch` exists
-		// base command
-		cmdFloat := `zellij run --name canvas --close-on-exit --floating --pinned true --height 20 --width 40 --x 100 --y 0 -- `
+		// TODO: can persistent flags be overwritten?
+		cmdFloat := fmt.Sprintf(`
+		zellij run --name canvas --close-on-exit --floating --pinned true \
+		--height %s \
+		--width %s \
+		--x %s \
+		--y %s \
+		-- `, floatHeight, floatWidth, floatX, floatY)
 		cmdFloat += `just watch`
-
-		// execute command
 		domovoi.ExecCmd("bash", "-c", cmdFloat)
 	},
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// execute prior main
 func init() {
 	rootCmd.AddCommand(zWatchCmd)
 }
