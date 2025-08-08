6c0dc81458a4a0f0bcb7565273d8902c6523a135
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-08 15:47:35 +0200

    update preset zellij resize cmd

--- cmd/cmdZellijResize.go
+++ cmd/cmdZellijResize.go
@@ -1,4 +1,3 @@
-// cmd/resize.go
 /*
 Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
 
@@ -23,6 +22,7 @@ import (
 	"fmt"
 
 	"github.com/DanielRivasMD/domovoi"
+	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
 	"github.com/ttacon/chalk"
 )
@@ -47,11 +47,44 @@ var zResizeCmd = &cobra.Command{
 		chalk.White.Color(chalk.Italic.TextStyle("--height")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<100%>")) + ` ` +
 		chalk.White.Color(chalk.Italic.TextStyle("--width")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<95%>")) + ` ` +
 		chalk.White.Color(chalk.Italic.TextStyle("--x")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<10>")) + ` ` +
-		chalk.White.Color(chalk.Italic.TextStyle("--y")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<0>")),
+		chalk.White.Color(chalk.Italic.TextStyle("--y")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<0>")) + `
+` +
+		// Preset examples
+		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` full` + `
+` +
+		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` half-left` + `
+` +
+		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` half-right`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Args:      cobra.MaximumNArgs(1),
+	ValidArgs: []string{"full", "half-left", "half-right"},
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 {
+			switch args[0] {
+			case "full":
+				floatHeight, floatWidth, floatX, floatY = "100%", "100%", "0", "0"
+			case "half-left":
+				floatHeight, floatWidth, floatX, floatY = "100%", "50%", "0", "0"
+			case "half-right":
+				floatHeight, floatWidth, floatX, floatY = "100%", "50%", "50%", "0"
+			default:
+				herr := horus.NewCategorizedHerror(
+					"resize",
+					"validation",
+					"invalid preset: expected one of [full, half-left, half-right]",
+					nil,
+					map[string]any{"got": args[0]},
+				)
+				horus.CheckErr(herr)
+				return
+			}
+		}
+
 		cmdResize := fmt.Sprintf(`
 		zellij action rename-pane canvas
 		zellij action change-floating-pane-coordinates --pane-id $ZELLIJ_PANE_ID \
