d15de42e73a042af04ff989ea4d44bafc3637163
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-10 11:53:37 +0200

    update completion cmd

--- cmd/cmdCompletion.go
+++ cmd/cmdCompletion.go
@@ -17,89 +17,91 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// declarations
-var ()
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// completionCmd represents the completion command
 var completionCmd = &cobra.Command{
-	Use:    "completion [bash|zsh|fish|powershell]",
+	Use:    "completion " + chalk.Dim.TextStyle(chalk.Italic.TextStyle("[bash|zsh|fish|powershell]")),
 	Hidden: true,
-	Short:  "Generate completion script.",
-	Long: `To load completions:
-
-Bash:
-
-$ source <(lou completion bash)
-
-# To load completions for each session, execute once:
-Linux:
-  $ lou completion bash > /etc/bash_completion.d/lou
-MacOS:
-  $ lou completion bash > /usr/local/etc/bash_completion.d/lou
-
-Zsh:
-
-# If shell completion is not already enabled in your environment you will need
-# to enable it.  You can execute the following once:
-
-$ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-# To load completions for each session, execute once:
-$ lou completion zsh > "${fpath[1]}/_lou"
-
-# You will need to start a new shell for this setup to take effect.
-
-Fish:
-
-$ lou completion fish | source
-
-# To load completions for each session, execute once:
-$ lou completion fish > ~/.config/fish/completions/lou.fish
-
-Powershell:
-
-PS> lou completion powershell | Out-String | Invoke-Expression
-
-# To load completions for every new session, run:
-PS> lou completion powershell > lou.ps1
-# and source this file from your powershell profile.
-`,
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+	Short:  "Generate completion script for various shells.",
+	Long:   helpCompletion,
 
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.ExactValidArgs(1),
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	Run: func(κ *cobra.Command, args []string) {
-		switch args[0] {
-		case "bash":
-			κ.Root().GenBashCompletion(os.Stdout)
-		case "zsh":
-			κ.Root().GenZshCompletion(os.Stdout)
-		case "fish":
-			κ.Root().GenFishCompletion(os.Stdout, true)
-		case "powershell":
-			κ.Root().GenPowerShellCompletion(os.Stdout)
-		}
-	},
+	Run: runCompletion,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var helpCompletion = fmt.Sprintf(`To load completions:
+
+Bash:
+
+  $ source <(%[1]s completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ %[1]s completion bash > /etc/bash_completion.d/%[1]s
+  # macOS:
+  $ %[1]s completion bash > $(brew --prefix)/etc/bash_completion.d/%[1]s
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it. You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ %[1]s completion zsh > "${fpath[1]}/_%[1]s"
+
+  # You will need to start a new shell for this setup to take effect.
+
+fish:
+
+  $ %[1]s completion fish | source
+
+  # To load completions for each session, execute once:
+  $ %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
+
+PowerShell:
+
+  PS> %[1]s completion powershell | Out-String | Invoke-Expression
+
+  # To load completions for every new session, run:
+  PS> %[1]s completion powershell > %[1]s.ps1
+  # and source this file from your PowerShell profile.
+`, rootCmd.Name())
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func runCompletion(cmd *cobra.Command, args []string) {
+	// Generate the appropriate shell completion script based on the provided argument.
+	switch args[0] {
+	case "bash":
+		horus.CheckErr(cmd.Root().GenBashCompletion(os.Stdout))
+	case "zsh":
+		horus.CheckErr(cmd.Root().GenZshCompletion(os.Stdout))
+	case "fish":
+		horus.CheckErr(cmd.Root().GenFishCompletion(os.Stdout, true))
+	case "powershell":
+		horus.CheckErr(cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout))
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
