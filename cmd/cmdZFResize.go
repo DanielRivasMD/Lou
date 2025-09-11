a5b17c6fd4d07eba32db3f1f34ca166724a4bd62
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-11 11:53:45 +0200

    update resize cmd

--- cmd/cmdZFResize.go
+++ cmd/cmdZFResize.go
@@ -24,65 +24,20 @@ import (
 	"github.com/DanielRivasMD/domovoi"
 	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
-	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var ()
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 var zfResizeCmd = &cobra.Command{
-	Use:   "resize",
-	Short: `anchor and resize a random floating pane`,
-	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) +
-		chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
-
-` +
-		`resize one random floating pane to a percentage of screen size and move it to an anchor point
-`,
-
-	Example: chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` ` +
-		chalk.White.Color(chalk.Italic.TextStyle("--height")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<100%>")) + ` ` +
-		chalk.White.Color(chalk.Italic.TextStyle("--width")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<95%>")) + ` ` +
-		chalk.White.Color(chalk.Italic.TextStyle("--x")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<10>")) + ` ` +
-		chalk.White.Color(chalk.Italic.TextStyle("--y")) + ` ` + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<0>")) + `
-` +
-		// Preset examples
-		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` full` + `
-` +
-		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` half-left` + `
-` +
-		chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("resize")) + ` half-right`,
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+	Use:     "resize",
+	Short:   "anchor and resize a random floating pane",
+	Long:    helpZFResize,
+	Example: exampleZFResize,
 
 	Args:      cobra.MaximumNArgs(1),
 	ValidArgs: validLayouts,
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	Run: func(cmd *cobra.Command, args []string) {
-
-		layoutName := "default"
-		if len(args) == 1 {
-			layoutName = args[0]
-		}
-
-		geom, err := resolveLayoutGeometry(layoutName)
-		horus.CheckErr(err)
-
-		cmdResize := fmt.Sprintf(`
-		zellij action rename-pane float
-		zellij action change-floating-pane-coordinates --pane-id $ZELLIJ_PANE_ID \
-		--height %s \
-		--width %s \
-		--x %s \
-		--y %s
-	`, geom.Height, geom.Width, geom.X, geom.Y)
-		domovoi.ExecCmd("bash", "-c", cmdResize)
-	},
+	Run: runResize,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,6 +45,31 @@ var zfResizeCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(zfResizeCmd)
 	zfCmd.AddCommand(zfResizeCmd)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func runResize(cmd *cobra.Command, args []string) {
+
+	layoutName := "default"
+	if len(args) == 1 {
+		layoutName = args[0]
+	}
+
+	geom, err := resolveLayoutGeometry(layoutName)
+
+	// TODO: update shell call
+	horus.CheckErr(err)
+
+	cmdResize := fmt.Sprintf(`
+		zellij action rename-pane float
+		zellij action change-floating-pane-coordinates --pane-id $ZELLIJ_PANE_ID \
+		--height %s \
+		--width %s \
+		--x %s \
+		--y %s
+	`, geom.Height, geom.Width, geom.X, geom.Y)
+	domovoi.ExecCmd("bash", "-c", cmdResize)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
