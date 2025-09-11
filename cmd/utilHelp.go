3caa4c25283a0e6abf34393695e9c82238a10aef
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-11 19:01:36 +0200

    update help util

--- cmd/utilHelp.go
+++ cmd/utilHelp.go
@@ -5,6 +5,8 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
+	"fmt"
+
 	"github.com/DanielRivasMD/domovoi"
 	"github.com/ttacon/chalk"
 )
@@ -23,19 +25,19 @@ var helpInit = domovoi.FormatHelp(
 	"",
 )
 
-var helpZFBat = domovoi.FormatHelp(
+var helpBat = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"<danielrivasmd@gmail.com>",
 	"View data in a floating "+chalk.Cyan.Color(chalk.Italic.TextStyle("zellij"))+" window using "+chalk.Cyan.Color(chalk.Italic.TextStyle("bat"))+" for a specified file",
 )
 
-var helpZFEza = domovoi.FormatHelp(
+var helpEza = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"<danielrivasmd@gmail.com>",
 	"View data in a floating "+chalk.Cyan.Color(chalk.Italic.TextStyle("zellij"))+" window using "+chalk.Cyan.Color(chalk.Italic.TextStyle("eza")),
 )
 
-var helpZFFloat = domovoi.FormatHelp(
+var helpFloat = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"<danielrivasmd@gmail.com>",
 	"Launch a floating "+chalk.Cyan.Color(chalk.Italic.TextStyle("zellij"))+" window using "+chalk.Cyan.Color(chalk.Italic.TextStyle("float"))+" with ease",
@@ -58,5 +60,32 @@ var helpZFWatch = domovoi.FormatHelp(
 	"<danielrivasmd@gmail.com>",
 	"launch a "+chalk.Cyan.Color(chalk.Italic.TextStyle("watcher"))+" on a floating "+chalk.Cyan.Color(chalk.Italic.TextStyle("zellij"))+" window with ease",
 )
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO: update edirot helps
+// helpEditorShort returns the one‐line Short description.
+func helpEditorShort(editor string) string {
+	return fmt.Sprintf("view data in a floating zellij window using %s", editor)
+}
+
+// helpEditorLong returns the multi‐line Long description.
+func helpEditorLong(editor string) string {
+	header := chalk.Green.Color(
+		chalk.Bold.TextStyle("Daniel Rivas "),
+	) +
+		chalk.Dim.TextStyle(
+			chalk.Italic.TextStyle("<danielrivasmd@gmail.com>"),
+		)
+
+	body := fmt.Sprintf(
+		"\n\nview data in a floating %szellij%s window using %s",
+		chalk.Cyan.Color(""),
+		chalk.Cyan.Color(""),
+		chalk.Cyan.Color(editor),
+	)
+
+	return header + chalk.Dim.TextStyle(body)
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
