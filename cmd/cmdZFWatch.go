90c55e23847d6229f2e1664778cf1029623be37b
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-11 11:53:40 +0200

    update watch cmd

--- cmd/cmdZFWatch.go
+++ cmd/cmdZFWatch.go
@@ -23,48 +23,39 @@ import (
 
 	"github.com/DanielRivasMD/domovoi"
 	"github.com/spf13/cobra"
-	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var ()
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 var zWatchCmd = &cobra.Command{
-	Use:   "watch",
-	Short: `launch watcher on a floating zellij window with ease`,
-	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) +
-		chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+	Use:     "watch",
+	Short:   "launch watcher on a floating zellij window with ease",
+	Long:    helpZFWatch,
+	Example: exampleZFWatch,
 
-` +
-		`launch a ` + chalk.Cyan.Color(chalk.Italic.TextStyle("watcher")) + ` on a floating ` + chalk.Cyan.Color(chalk.Italic.TextStyle("zellij")) + ` window with ease
-`,
-
-	Example: chalk.White.Color("lou") + ` ` + chalk.White.Color(chalk.Bold.TextStyle("watch")),
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: test whether `just watch` exists
-		// TODO: can persistent flags be overwritten?
-		cmdFloat := fmt.Sprintf(`
-		zellij run --name watch --close-on-exit --floating --pinned true \
-		--height %s \
-		--width %s \
-		--x %s \
-		--y %s \
-		-- `, floatHeight, floatWidth, floatX, floatY)
-		cmdFloat += `just watch`
-		domovoi.ExecCmd("bash", "-c", cmdFloat)
-	},
+	Run: runWatch,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func init() {
 	rootCmd.AddCommand(zWatchCmd)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func runWatch(cmd *cobra.Command, args []string) {
+	// TODO: test whether `just watch` exists
+	// TODO: can persistent flags be overwritten?
+	cmdFloat := fmt.Sprintf(`
+		zellij run --name watch --close-on-exit --floating --pinned true \
+		--height %s \
+		--width %s \
+		--x %s \
+		--y %s \
+		-- `, floatHeight, floatWidth, floatX, floatY)
+	cmdFloat += `just watch`
+	domovoi.ExecCmd("bash", "-c", cmdFloat)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
