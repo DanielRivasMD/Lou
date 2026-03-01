dd29c6b9b17413b2186c96eb2914e5e365ecd634
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-01 13:31:22 +0100

    refactor path cmd

--- cmd/cmdPath.go
+++ cmd/cmdPath.go
@@ -20,7 +20,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 
@@ -31,30 +30,20 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// pathCmd prints one of the standard shell path variables.
-// Usage: lou path [path|fpath|gopath]  (defaults to PATH).
-var pathCmd = &cobra.Command{
-	Use:     "path [which]",
-	Short:   "Print entries of PATH, FPATH, or GOPATH",
-	Long:    helpPath,
-	Example: examplePath,
-
-	Args:              cobra.MaximumNArgs(1),
-	ValidArgs:         []string{"path", "fpath", "gopath"},
-	ValidArgsFunction: completePathVars,
-
-	Run: runPath,
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 func init() {
+	pathCmd := MakeCmd("path", runPath,
+		WithArgs(cobra.MaximumNArgs(1)),
+		WithValidArgs([]string{"path", "fpath", "gopath"}),
+	)
+	pathCmd.ValidArgsFunction = completePathVars
 	rootCmd.AddCommand(pathCmd)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func runPath(cmd *cobra.Command, args []string) {
+	const op = "lou.path"
+
 	var envvar string
 	switch {
 	case len(args) == 0:
@@ -66,8 +55,12 @@ func runPath(cmd *cobra.Command, args []string) {
 	case strings.EqualFold(args[0], "gopath"):
 		envvar = "GOPATH"
 	default:
-		fmt.Fprintf(os.Stderr, "invalid option %q, must be one of [path, fpath, gopath]\n", args[0])
-		os.Exit(1)
+		horus.CheckErr(
+			fmt.Errorf("invalid option %q, must be one of [path, fpath, gopath]", args[0]),
+			horus.WithOp(op),
+			horus.WithCategory("USAGE_ERROR"),
+			horus.WithExitCode(1),
+		)
 	}
 	printEnvPaths(envvar)
 }
@@ -88,9 +81,16 @@ func completePathVars(cmd *cobra.Command, args []string, toComplete string) ([]s
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func printEnvPaths(envvar string) {
+	const op = "lou.path.print"
+
 	cmd := exec.Command("sh", "-c", "echo $"+envvar)
 	out, err := cmd.Output()
-	horus.CheckErr(err, horus.WithOp("path.print"), horus.WithMessage("reading "+envvar))
+	horus.CheckErr(
+		err,
+		horus.WithOp(op),
+		horus.WithMessage("reading "+envvar),
+		horus.WithCategory("SYS_CMD"),
+	)
 
 	parts := strings.Split(strings.TrimSpace(string(out)), ":")
 	domovoi.LineBreaks(true)
